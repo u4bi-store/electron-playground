96f055518b2fdd61388ce3708fb9ce819cc39bf7
myungjaeyu <rootcode10@gmail.com>
2018-10-09 09:50:33 +0900

    Refactoring and comments update.

--- main.js
+++ main.js
@@ -1,50 +1,25 @@
-// Modules to control application life and create native browser window
+/* 생명주기는 electron.app을 통해 관리되며 electron.BrowserWindow 창들이 생성 됨. */
 const {app, BrowserWindow} = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+var mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools() /* 개발자 도구 활성화 */
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    /* 프레임이 제거되었을 때 발생 */
+    mainWindow.on('closed',  _ => mainWindow = null)
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+/* 초기화 완료 시 발생 */
+app.on('ready', _ => createWindow())
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+/* 모든 프레임이 제거되었을 때 발생 */
+app.on('window-all-closed', _ => process.platform !== 'darwin' && app.quit()) /* cmd + Q 프로그램 종료 이벤트에도 호출 */
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+/* 프레임이 활성화되었을 때 발생 */
+app.on('activate', _ => mainWindow === null && createWindow()) /* 닫혔을 때 dock에 활성화된 아이콘 누를 시에도 활성 */
